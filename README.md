4a9d679c0b404587164ede27d543f5abbe2cb1ca
jeanbaptisteguillois <jguilloi@amazon.fr>
2021-09-21 15:04:08 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,17 +1,50 @@
-## My Project
+# Getting Started with the React Cors Application
 
-TODO: Fill this README out!
+## Getting started
 
-Be sure to:
+In the project directory, run the following command to install all modules:
+`npm install`
 
-* Change the title in this README
-* Edit your repository description on GitHub
+then start the application locally using the following command:
+`yarn start`
 
-## Security
+## Deploying to AWS
 
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+In order to deploy to AWS, you have to take the following steps:
+1. Deploy the CloudFormation Template from the project (`react-cors-spa-stack.yaml`) using AWS CLI or AWS Console
+2. Once your stack is deployed, from the "Output" tab, identify the "APIEndpoint" URL as well as the S3 "Bucket" name
+3. Copy the API endpoint URL identified at step 2 and paste it in the App.js line 26
+4. Build the (using `yarn build`) app for distribution
+5. Upload the content of the `build` folder into the S3 bucket identified at step 2
+6. Access the application through the CloudFront distribution created at step 1
+
+## Available Scripts
+
+In the project directory, you can run:
+
+`yarn start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+`yarn test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+`yarn build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
 ## License
 
-This library is licensed under the MIT-0 License. See the LICENSE file.
-
+This sample application is licensed under [the MIT-0 License](https://github.com/aws/mit-0).
---
+++ src/App.js
@@ -0,0 +1,53 @@
+/*
+** React CORS friendly Single Page Application - https://github.com/jeanbaptisteguillois/react-cors-spa 
+© 2021 Jean-Baptiste Guillois, Inc. or its affiliates. All Rights Reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import logo from './logo.svg';
+import logoS3 from './logoS3.png';
+import logoCF from './logoCloudFront.png';
+import './App.css';
+import { useFetch } from "react-async"
+
+// To be replaced by the endpoint of the API deployed through the CloudFormation Template
+const APIEndPoint = 'to be replaced with your api endpoint here'
+
+function App() {
+  return (
+    <div className="App">
+        <header className="App-header">
+            <APIResult />
+            <img src={logo} className="App-MainLogo" alt="logo" />
+        </header>
+        <p>This react-based application is hosted in an S3 bucket exposed through a CloudFront distribution</p>
+        <div className="logos">
+            <img src={logoS3} className="App-logoR2L" alt="logo S3" />
+            <img src={logoCF} className="App-logoL2R" alt="logo CloudFront" />
+        </div>
+    </div>
+  );
+}
+
+const APIResult = () => {
+  const { data, error } = useFetch(APIEndPoint, {
+    headers: { accept: "application/json" },
+  })
+  if (error) return <p>{error.message}</p>
+  if (data) return <p>{data.message}</p>
+  return null
+}
+
+export default App;
